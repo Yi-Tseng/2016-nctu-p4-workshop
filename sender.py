af0bab84ac1f8d44dd4b74cf16872702c8075661
Yi Tseng <a86487817@gmail.com>
2016-07-18 15:55:50 +0800

    add counter to sender

--- sender.py
+++ sender.py
@@ -6,8 +6,10 @@ from time import sleep
 
 def main():
 
+    count = 1
     while(1):
-        p = "\x00\x00\x00\x01Hello"
+        p = "\x00\x00\x00\x01Hello%d" % (count, )
+        count += 1
         sendp(p, iface = "eth0")
         sleep(1)
 

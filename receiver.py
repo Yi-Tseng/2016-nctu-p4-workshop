3f7cd7056146f7a9419760e7d2f779280f54fb60
Yi Tseng <a86487817@gmail.com>
2016-07-18 15:22:11 +0800

    Add more prompt for receiver

--- receiver.py
+++ receiver.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
+import sys
+import struct
 from scapy.all import sniff
-
-
+to_hex = lambda x:"".join([hex(ord(c))[2:].zfill(2) for c in x])
 
 def handle_pkt(pkt):
     pkt = str(pkt)
-    print pkt
+    raw_hex = to_hex(pkt)
+    print "Received %d bytes" % (len(pkt), )
+    print "Hex data: %s" % (raw_hex, )
+    src, dst, data = struct.unpack("!2B2Bs", pkt)
+    sys.stdout.flush()
 
 def main():
     sniff(iface = "eth0", prn = lambda x: handle_pkt(x))
